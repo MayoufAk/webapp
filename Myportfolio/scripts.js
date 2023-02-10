63e91baef1165590ecc0cbaa6047b413d2bec0f9
MayoufAk <102841858+MayoufAk@users.noreply.github.com>
2023-02-10 01:14:45 -0500

    Add files via upload

---
+++ Myportfolio/scripts.js
@@ -0,0 +1,21 @@
+// const switchElement=document.querySelector('.lop')
+// switchElement.addEventListener('click',()=>{
+//   document.body.classList.toggle('pop')
+// })
+    // Sticky navigation
+    // let navbar=$("navbar");//stored navbar in let variable
+    //  $(window).scroll(function()
+    //  {                           // call jQueury object by $
+    //     let oTop=$(".section-2").offset().top-window.innerHeight;
+    //     if($(window).scrollTop()>oTop){
+    //         navbar.addClass("sticky");
+    //     }else{
+    //         navbar.removeClass("sticky");
+    //     }                                                                  //offset allows us to retrieve the current posiion if the element relating to the document
+
+    // })
+    // let navbar=$(".navabar");
+    // $(Window).scroll(function(){
+    //     console.log(window.innerHeight)
+
+    // });
